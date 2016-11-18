24bfa985ec4faed900c4a77471b3896bdefdbc1c
root <mishnade@gmail.com>
2016-11-17 16:54:17 -0800

    styling

--- content/team-member-todd-wilson.php
+++ content/team-member-todd-wilson.php
@@ -5,13 +5,13 @@
 	</article>
 	<article class="bio">
 		<ul class="bio-wrap">
-			<li class="img todd-img"><img src="images/todd-wilson-large.jpg" /></li>
+			<li class="img"><img src="images/todd-wilson-large.jpg" /></li>
 			<li class="txt-area">
 				<p><span class="firstcharacter">T</span>odd Wilson is President of Grow Capital Management and leads GCM’s business planning and operations. Todd brings more than 18 years of investment management experience to Grow Capital Management. As President, Todd is a player-coach, working directly with his clients to address their investment and wealth management needs, and with the portfolio management team to help them build and manage sustainable practices.</p>
 
 				<p>Prior to joining Grow Capital Management, Todd was a Vice President and Portfolio Manager at BNY Mellon in Menlo Park. Todd joined BNY Mellon in 2016 through the acquisition of Atherton Lane Advisers, where he was a Portfolio Manager for 8 years. Prior to working at Atherton Lane Advisers, Todd spent over 10 years as a Financial Advisor with Merrill Lynch in San Francisco. Todd began his career at Aetna, designing and implementing employee benefits plans for mid-market companies in Northern California.</p>
 
-				<p>Todd received a BA in Economics from the University of California at Berkeley, focusing on Investments and Econometrics. Todd has significant experience with private company investing, stock option exercises, restricted stock transactions, and 10b5-1 trading plans.</p>
+				<p>Todd received a BA in Economics from the University of California at Berkeley, focusing on Investments and Econometrics. </p>
 
 				<p>On the weekends, Todd can usually be found in the mountains or near a body of water with his two young children.</p>
 			</li>
