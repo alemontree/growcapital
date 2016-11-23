55128418c0718640445606ac6fd63a9005a9941a
root <mishnade@gmail.com>
2016-11-23 11:37:43 -0800

    change link to schwab

--- content/section-clients.php
+++ content/section-clients.php
@@ -1,12 +1,12 @@
 <section id="section-clients">
 	<article class="heading-wrap">
 		<h1 class="page-heading">Client Login</h1>
-		<p class="sub-heading">Manage your account and view your investments</p>
+		<p class="sub-heading">View your portfolio and access your reports</p>
 		<div class="content">
-			<p>Our client portal is currently been developed to offer you a more personal experience. In the mean time you can still login to Schwab to view your account information.</p>
+			<p>Our client portal is currently being developed to offer you a more personal experience. In the meantime, you can login to Charles Schwab to view your account information.</p>
 			<div class="button-wrap">
-	   			<a href="" class="button-green button">Schwab Login</a>
-	   		</div>
-	   	</div>
+   			<a href="http://www.schwaballiance.com" target="_blank" class="button-green button">Schwab Login</a>
+   		</div>
+   	</div>
 	</article>
 </section>
